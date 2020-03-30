31f88ef0cc96a4d1bb0abbc8475a076402c0e41a
Glitch (hello-express) <none>
2020-03-30 04:18:02 +0000

    🐘🏀 Checkpoint ./komutlar/seviysıra.js:3756271/173

--- komutlar/seviysıra.js
+++ komutlar/seviysıra.js
@@ -18,17 +18,18 @@ exports.run = async (client, msg, args) => {
         const top10 = sorted.splice(0, msg.guild.members.size)
         const mappedName = top10.filter(o => !o.bot).map(s => s.user.id);
         const mappedLevel = top10.filter(o => !o.bot).map(s => db.fetch(`seviye_${s.user.id + msg.guild.id}`) || 0)
+        const emoji = client.emojis.get('694036610093940738');
 
         const mappedID = top10.map(s => s.user.id);
         for(var i = 0; i < 10; i++) {
             var lvl = mappedLevel[i]
       
             if(msg.author.id === mappedID[i]) {
-                str += `[${i + 1}] > <@${mappedName[i]}>\n  Level: ${lvl} \n\n`
+                str += `**[${i + 1}]** ${emoji} <@${mappedName[i]}>\n  **Level:** ${lvl} \n\n`
             }
 
             if(msg.author.id !== mappedID[i]) {
-                str += `[${i + 1}] > <@${mappedName[i]}>\n  Level: ${lvl} \n\n`
+                str += `**[${i + 1}]** ${emoji} <@${mappedName[i]}>\n  **Level:** ${lvl} \n\n`
             }
         }
 
