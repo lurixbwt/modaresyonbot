6a45643220ba98acbe777d3efff2786482f985a4
Glitch (hello-express) <none>
2020-04-18 17:00:54 +0000

    🚂📙 Checkpoint ./komutlar/rolver.js:3756271/10

--- komutlar/rolver.js
+++ komutlar/rolver.js
@@ -1,11 +1,11 @@
 const Discord = require('discord.js');
 exports.run = (client, message, args) => {
-    let rol = message.guild.roles.find(`name`, "sᴀʀɪ");
-    let kullanıcı = message.guild.member(message.mentions.users.first());
+    //let rol = message.guild.roles.find(`name`, "sᴀʀɪ");
+   // let kullanıcı = message.guild.member(message.mentions.users.first());
      
-if (!kullanıcı) return message.reply("Bir kullanıcıdan bahsetmelisin.");
-    kullanıcı.addRole(rol.id);
-  message.reply("Rol Eklendi");
+//if (!kullanıcı) return message.reply("Bir kullanıcıdan bahsetmelisin.");
+ //   kullanıcı.addRole(rol.id);
+ // message.reply("Rol Eklendi");
 };
 
 exports.conf = {
