359c744d76a8eda677591d3ce73fe810c53e51a7
Glitch (hello-express) <none>
2020-05-02 23:59:49 +0000

    🎩🏏 Checkpoint ./komutlar/yardım.js:3756271/5936

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -1,28 +1,46 @@
 const Discord = require("discord.js");
-const ayarlar = require("../ayarlar.json");
-const prefix = ayarlar.prefix;
-exports.run = (client, message, args) => {
-  message.delete();
-    const embed = new Discord.RichEmbed()
-     .setColor('RANDOM')
-    .setDescription(`
-Müziği Başlat 1
-Müziği Durdur 2
-Müziği Başlat 1
+const db = require("quick.db");
+module.exports.run = async (bot, message, args) => {
+  let prefix = (await db.fetch(`prefix_${message.guild.id}`)) || "!";
 
-`,true)
+
+  const embed = new Discord.RichEmbed()
+    .setColor("BLACK")
+    .setDescription(
+      ` Müziği Durdurmayı Onaylıyormusunuz?`
+    )
+  .setFooter(bot.user.username, bot.user.avatarURL)
+  message.channel.send(embed).then(async function(sentEmbed) {
+    const emojiArray = ["✅"];
+    const filter = (reaction, user) =>
+      emojiArray.includes(reaction.emoji.name) && user.id === message.author.id;
+    await sentEmbed.react(emojiArray[0]).catch(function() {});
+    var reactions = sentEmbed.createReactionCollector(filter, {
+      time: 30000
+    });
+    reactions.on("end", () => sentEmbed.edit("İşlem iptal oldu!"));
+    reactions.on("collect", async function(reaction) {
+      if (reaction.emoji.name === "✅") {
+        message.channel.send(
+          `İşlem onaylandı! Müzik Durduruldu.`
+        );
+
+        db.delete(`davet_${message.author.id}_${message.guild.id}`);
+      }
+    });
+  });
 };
 
-exports.conf = {
-  enabled: true,
-  guildOnly: false,
+module.exports.conf = {
   aliases: ["yardım"],
-  permLevel: 0,
-  kategori: "üye"
+  permLevel: 2,
+  enabled: true,
+  guildOnly: true,
+  kategori: "moderasyon"
 };
 
-exports.help = {
+module.exports.help = {
   name: "yardım",
-  description: "Yardım MENÜSÜ Açılır.",
+  description: "davet-sıfırla",
   usage: "yardım"
 };
