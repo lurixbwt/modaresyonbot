76350c7275a9c358bcbbc9a4ffd026128bf6e1f7
Glitch (hello-express) <none>
2020-05-04 17:41:40 +0000

    🚈📷 Checkpoint ./komutlar/jail.js:3756271/366

--- komutlar/jail.js
+++ komutlar/jail.js
@@ -5,22 +5,21 @@ const ms = require('ms');
 exports.run = async(client, message, args, prefix, ayar, emoji) => {
   // !sesmute @etiket 5m
   let LoZUser = message.mentions.members.first();
-  let sure = args[1];
   let sebep = args.slice(2).join(' ');
   let logKanali = "706901358598684702"; // LOG KANALININ ID
   let cezaliRolu = "706901136807952485"; // CEZALI ROLÜNÜN ID
+  let süre = args.slice(1).join(' ').replace('gün'.toLowerCase(), 'd').replace('saat'.toLowerCase(), 'h').replace('dakika'.toLowerCase(), 'm').replace('saniye'.toLowerCase(), 's');
   
-  if(!sure){
-  if (!LoZUser || !sure || !sebep) return message.reply('Doğru kullanmalısın!\n' +this.help.usage);
+  
+  if (!LoZUser || !süre || !sebep) return message.reply('Doğru kullanmalısın!\n' +this.help.usage);
   LoZUser.addRole(cezaliRolu);
-  message.channel.send(`${LoZUser.displayName} adlı üye ses kanalında **${sure}** kadar susturuldu!`);
-  client.channels.get(logKanali).send(`${LoZUser} adlı üye ses kanalında **${sure}** kadar, **${sebep}** nedeniyle susturuldu!`);
-   } else {
-  setTimeout(() => {
+  message.channel.send(`${LoZUser.displayName} adlı üye ses kanalında **${süre}** kadar susturuldu!`);
+  client.channels.get(logKanali).send(`${LoZUser} adlı üye ses kanalında **${süre}** kadar, **${sebep}** nedeniyle susturuldu!`);
+    setTimeout(() => {
     LoZUser.removeRole(cezaliRolu);
     message.channel.send(`${LoZUser.displayName} adlı üyenin ses susturması kaldırıldı!`);
     client.channels.get(logKanali).send(`${LoZUser.displayName} adlı üyenin ses susturması kaldırıldı!`);
-  }, ms(sure));}
+  }, ms(süre));
 };
 
 exports.conf = {
